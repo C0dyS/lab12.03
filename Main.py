23969ccb833b421c8f9b3b70585951f25d3b36ab
C0dyS <sashaculle@gmail.com>
2024-03-12 19:11:54 +0100

    thrid task

--- Main.py
+++ Main.py
@@ -21,3 +21,17 @@ class Cricle:
 
 circle_1 = Cricle(10)
 circle_1.calculate_area()
+
+class Book:
+    def __init__(self,title,author,genre):
+        self.title = title
+        self.author = author
+        self.genre = genre
+
+    def display_info(self):
+        print (f"Назва: {self.title}, Автор: {self.author}, Жанр: {self.genre}")
+
+book1 = Book('a','b','c')
+book1.display_info()
+
+
